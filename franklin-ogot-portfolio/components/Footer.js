b50526705d48a9e9e3f56fc41951708f049eac7a
Frankyy <franklinogot11@gmail.com>
2025-05-16 23:34:22 +0800

    Delete franklin-ogot-portfolio directory

--- franklin-ogot-portfolio/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 text-center py-6 text-sm text-gray-500">
-      <p>&copy; {new Date().getFullYear()} Franklin Ogot. All rights reserved.</p>
-    </footer>
-  )
-}
